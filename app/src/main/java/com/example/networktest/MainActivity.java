d9cf1f030295281dc629ee0e185734bcc2400fcb
Durmus Güler <d1gueler.edu.aau.at>
2021-03-14 09:23:57 +0100

    a

--- app/src/main/java/com/example/networktest/MainActivity.java
+++ app/src/main/java/com/example/networktest/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.networktest;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.BufferedReader;
 import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClick(View view) {
+    public void onClick(View v) {
 
 
-        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView textView2 = (TextView) findViewById(R.id.matrikelnummer);
         EditText editText = (EditText) findViewById(R.id.editText);
         int number = Integer.parseInt(editText.getText().toString());
         int summe = 0;
@@ -37,24 +33,25 @@ public class MainActivity extends AppCompatActivity {
             number = number / 10;
         }
 
-        String s=(Integer.toBinaryString(summe));
+        String bin=(Integer.toBinaryString(summe));
 
 
-        textView2.setText(s);
-        super_good_server_method();
+        textView2.setText(bin);
+        server();
     }
 
 
     String text;
 
-    public void super_good_server_method(){
+    public void server(){
 
-        Thread t=new Thread(new Runnable() {
+        Thread p=new Thread(new Runnable() {
             @Override
             public void run() {
 
                 EditText editText = (EditText) findViewById(R.id.editText);
                 Socket socket = null;
+
                 try {
                     socket = new Socket("se2-isys.aau.at", 53212);
                     OutputStream raus = socket.getOutputStream();
@@ -68,31 +65,33 @@ public class MainActivity extends AppCompatActivity {
                 } catch (UnknownHostException e) {
                     text="Uknown Host...";
                     e.printStackTrace();
+
                 } catch (IOException e) {
                     text="IOProbleme...";
                     e.printStackTrace();
+
                 } finally {
                 }
+
                 if (socket != null)
                     try {
                         socket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
             }
 
         });
-        t.start();
+        p.start();
         try{
-            t.join();
+            p.join();
         }catch (InterruptedException e){e.printStackTrace();}
         runOnUiThread(new Runnable() {
 
             @Override
             public void run() {
-                TextView textView3 = (TextView) findViewById(R.id.textView3);
-                textView3.setText(text);
+                TextView Serverj = (TextView) findViewById(R.id.ServerJ);
+                Serverj.setText(text);
             }
         });
     }
